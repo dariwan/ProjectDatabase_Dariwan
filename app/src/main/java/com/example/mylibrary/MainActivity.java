d7d1f58165525d885d00bfa71878eefd0f692114
dariwan1607@gmail.com <%..Katasandi16..%>
2021-08-16 22:45:27 +0700

    first commit

---
+++ app/src/main/java/com/example/mylibrary/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.mylibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnStore, btnGetall;
+    private EditText etname;
+    private DatabasesHelper databaseHelper;
+    private TextView tvnames;
+    private ArrayList<String> arrayList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        databaseHelper = new DatabasesHelper(this);
+        tvnames = findViewById(R.id.tvnames);
+        btnStore = findViewById(R.id.btnstore);
+        btnGetall = findViewById(R.id.btnget);
+        etname = findViewById(R.id.etname);
+
+        btnStore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                databaseHelper.addStudentDetail(etname.getText().toString());
+                etname.setText("");
+                Toast.makeText(MainActivity.this, "Stored Successfully!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    btnGetall.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            arrayList = databaseHelper.getAllStudentsList();
+            tvnames.setText("");
+            for (int i = 0; i < arrayList.size(); i++){
+                tvnames.setText(tvnames.getText().toString()+","+arrayList.get(i));
+            }
+        }
+    });
+    }
+
+}
